3407b70151ea55abfad744c46c80cfda1788318b
HLazo <heberlazo@gmail.com>
2021-08-03 15:07:02 -0500

    Added id to books

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import store from '../reducers/store';
 import * as actions from '../actions/book';
 
@@ -6,10 +6,6 @@ const BooksList = () => {
   store.dispatch(actions.bookAdded('book 1', 'description of book 1', 'horror'));
   store.dispatch(actions.bookAdded('book 2', 'description of book 2', 'action'));
   store.dispatch(actions.bookAdded('book 3', 'description of book 3', 'war'));
-
-  useEffect(() => {
-
-  }, []);
 
   const getBooks = (store.getState());
 
@@ -20,6 +16,8 @@ const BooksList = () => {
           <h5 className="card-title">{book.title}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{book.category}</h6>
           <p className="card-text">{book.description}</p>
+          <span>ID: </span>
+          <span>{book.id}</span>
         </div>
       </div>
     ))
