498d15c69ebadc6bcec3c6d7c57ee0afd33bba16
HLazo <heberlazo@gmail.com>
2021-08-03 18:27:23 -0500

    Added unnecesary files

--- src/reducers/createStore.js
+++
@@ -1,6 +0,0 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import reducer from './books';
-
-// export default function configureStoreApp() {
-//   return configureStore(reducer);
-// }
