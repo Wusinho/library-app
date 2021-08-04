b023a36fb261bd8a011605e84b972f67eacc559a
HLazo <heberlazo@gmail.com>
2021-08-04 09:35:59 -0500

    Added categories into cotainer and render it

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
+import categories from './categories';
 
-function BookForm() {
-  return (
-    <form>
-      <div className="input-group mb-3">
-        <input type="text" className="form-control" placeholder="Book title" />
-        <input type="text" className="form-control" placeholder="Description" />
-        <select className="form-control" id="input3">
-          <option defaultValue>Choose a Category</option>
-          <option>Action</option>
-          <option>Biography</option>
-          <option>History</option>
-          <option>Horror</option>
-          <option>Kids</option>
-          <option>Learning</option>
-          <option>Sci-Fi</option>
-        </select>
-        <button className="btn btn-success" type="button" id="input5">Add Book</button>
-      </div>
-    </form>
-  );
-}
+const BookForm = () => (
+  <form>
+    <div className="input-group mb-3">
+      <input type="text" className="form-control" placeholder="Book title" />
+      <input type="text" className="form-control" placeholder="Description" />
+      <select className="form-control" id="input3">
+        <option defaultValue>Choose a Category</option>
+        {categories.map((cat) => (
+          <option key={cat.id} value="cat">{cat.cat}</option>
+        ))}
+      </select>
+      <button className="btn btn-success" type="button" id="input5">Add Book</button>
+    </div>
+  </form>
+);
 
 export default BookForm;
