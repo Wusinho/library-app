66bb135d65cfc06cc109f9f016653da82aea322b
HLazo <heberlazo@gmail.com>
2021-08-04 09:50:27 -0500

    Added id generator to book id

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import * as actions from '../actions/booksActions';
 
 export default function reducer(state = [], action) {
@@ -5,7 +7,7 @@ export default function reducer(state = [], action) {
     return [
       ...state,
       {
-        id: Math.floor(Math.random() * 10000),
+        id: uuidv4().slice(0, 6),
         title: action.payload.title,
         description: action.payload.description,
         category: action.payload.category,
