6eef2decbb474207dd8add75fcb8d3ab15f3b223
HLazo <heberlazo@gmail.com>
2021-08-05 15:30:54 -0500

    Added style to cards

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -6,7 +6,7 @@ const BooksList = () => {
   // console.log(books);
   return (
     books.map((book) => (
-      <div className="card my-2" key={book.id}>
+      <div className="card my-3" key={book.id}>
         <div className="card-body">
           <h5 className="card-title">{book.title}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{book.category}</h6>
@@ -14,7 +14,7 @@ const BooksList = () => {
           <span>ID: </span>
           <span>{book.id}</span>
         </div>
-        <button className="btn btn-danger" type="button">Remove Book</button>
+        <button className="btn btn-danger m-2" type="button">Remove Book</button>
       </div>
     ))
   );
