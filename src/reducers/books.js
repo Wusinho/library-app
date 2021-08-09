fab4d93c9e20d523a81c60345adeef5d48ba1887
HLazo <heberlazo@gmail.com>
2021-08-09 11:20:10 -0500

    Correted AddBooks function

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,5 +1,5 @@
-// import { v4 as uuidv4 } from 'uuid';
-import { createSlice, createSelector } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
+import { createSlice } from '@reduxjs/toolkit';
 
 export const bookSlice = createSlice({
   name: 'book',
@@ -9,18 +9,17 @@ export const bookSlice = createSlice({
   reducers: {
     addBook: (state, action) => {
       state.list.push({
-        id: action.payload.id,
+        id: uuidv4().slice(0, 5),
         name: action.payload.name,
         description: action.payload.description,
         category: action.payload.category,
       });
     },
+    removeBook: (state, action) => {
+      state.filter((book) => book.id !== action.payload.id);
+    },
   },
 });
-
-export const getBookList = () => createSelector(
-  (state) => state.entities.bookSlice.list,
-);
 
 export const { addBook } = bookSlice.actions;
 
