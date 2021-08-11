a37bd7d06023e772cec21b3ade0adfc3497ec7f7
HLazo <heberlazo@gmail.com>
2021-08-10 21:45:57 -0500

    Removed redux toolkit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import BookForm from './BookForm';
 import BookList from './BooksList';
-import configureStoreApp from '../reducers/configureStore';
-
-const store = configureStoreApp();
 
 const App = () => (
-  <Provider store={store}>
+  <div>
     <BookForm />
     <BookList />
-  </Provider>
+  </div>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/App.css';
+import reducer from './reducers/reducer';
+
+const store = createStore(reducer,
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
